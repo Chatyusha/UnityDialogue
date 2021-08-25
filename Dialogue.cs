3319cb1b74e399fbab398117b57b5c2bc53c8529
Chatyusha <hirounicat76@gmail.com>
2021-08-26 01:10:24 +0900

    update

--- Dialogue.cs
+++ Dialogue.cs
@@ -2,20 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class Dialogue : MonoBehaviour
 {
-    public void TextDialogue(Text text,string message, float time)
+    public void TextDialogue(Text text,string message, float time, Action beforeFunc=null, Action afterFunc = null)
     {
-        StartCoroutine(IEDialogue(text, message, time));
+        
+        StartCoroutine(IEDialogue(text, message, time,beforeFunc,afterFunc));
     }
 
-    IEnumerator IEDialogue(Text text, string message, float time)
+    IEnumerator IEDialogue(Text text, string message, float time, Action beforeFunc = null, Action afterFunc=null)
     {
-        foreach(var c in message)
+        if (beforeFunc != null)
+        {
+            beforeFunc();
+        }
+        foreach (var c in message)
         {
             text.text += c;
             yield return new WaitForSeconds(time / message.Length);
         }
+        if (afterFunc != null)
+        {
+            afterFunc();
+        }
+    }
+
+    internal void TextDialogue(Text text, string msg, int v1, object v2)
+    {
+        throw new NotImplementedException();
     }
 }
